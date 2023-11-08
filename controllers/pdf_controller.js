1d11a2994207babe61fd676a6e2921f4797edefd
premnarayanp <premnarayanpatel223@gmail.com>
2023-11-08 20:05:27 +0530

    create controller and route for load/view padf, download pdf,delete pdf and get pdf posts

--- controllers/pdf_controller.js
+++ controllers/pdf_controller.js
@@ -39,3 +39,114 @@ module.exports.uploadPDF = async function(req, res) {
     }
 
 }
+
+
+//Load My PDF
+module.exports.loadPDF = async function(req, res) {
+    try {
+        const PDFs = await PDFfile.findById({ _id: req.params.pdf_id });
+        if (PDFs) {
+
+            if (PDFs.user.equals(req.user._id)) {
+                //res.sendFile(path.join(__dirname, '../uploads/posts/pdf/') + PDFs.fileName);
+
+                // fs.readFile('./uploads/posts/pdf/' + PDFs.fileName, function(err, data) {
+                //     res.contentType("application/pdf");
+                //     console.log("======data=======", data);
+                //     res.send(data);
+                // });
+
+                const data = fs.readFileSync('./uploads/posts/pdf/' + PDFs.fileName);
+                res.contentType("application/pdf");
+                res.send(data);
+
+            } else {
+                return res.json({ success: false, msg: "You could not view this pdf..", data: { PDFs: null } });
+
+            }
+
+        } else {
+            return res.json({ success: false, msg: "Could not found pdf,Pdf not available..", data: { PDFs: null } });
+        }
+
+    } catch (error) {
+        console.log('error in finding Pdf', error);
+        return res.json({ success: false, msg: "Internal server Error..", data: null });
+
+    }
+}
+
+module.exports.listOfPdf = async function(req, res) {
+
+    try {
+        // const pdfFiles = await PDFfile.find({ user: req.user._id }).populate('user');
+        const pdfFiles = await PDFfile.find({ user: req.user._id }).populate('pdfVersion');
+        if (pdfFiles) {
+            return res.json({ success: true, msg: "success", data: { pdfList: pdfFiles } });
+        } else {
+            return res.json({ success: false, msg: "There is nothing pdf", data: null });
+        }
+
+
+    } catch (error) {
+        return res.json({ success: false, msg: "Internal server Error..", data: null });
+    }
+
+}
+
+
+//Download My PDF
+module.exports.downloadPDF = async function(req, res) {
+    // console.log("============inside download==============")
+    try {
+        const PDFs = await PDFfile.findById({ _id: req.params.pdf_id });
+        if (PDFs) {
+
+            if (PDFs.user.equals(req.user._id)) {
+
+                //res.sendFile(path.join(__dirname, '../uploads/posts/pdf/') + PDFs.fileName);
+                res.download('./uploads/posts/pdf/' + PDFs.fileName);
+
+            } else {
+                return res.json({ success: false, msg: "You could not download this pdf..", data: { PDFs: null } });
+
+            }
+
+        } else {
+            return res.json({ success: false, msg: "Could not found pdf,Pdf not available..", data: { PDFs: null } });
+        }
+
+    } catch (error) {
+        console.log('error in finding Pdf', error);
+        return res.json({ success: false, msg: "Internal server Error..", data: null });
+
+    }
+}
+
+//delete My PDF
+module.exports.deletePDF = async function(req, res) {
+    try {
+        const PDFs = await PDFfile.findById({ _id: req.params.pdf_id });
+        if (PDFs) {
+
+            if (PDFs.user.equals(req.user._id)) {
+                await PDFfile.findByIdAndDelete({ _id: req.params.pdf_id });
+                //await PdfVersion.deleteMany({ pdf: req.params.pdf_id });
+                fs.unlinkSync('./uploads/posts/pdf/' + PDFs.fileName);
+                return res.json({ success: true, msg: " file successfully deleted", data: { pdf: PDFs } });
+
+            } else {
+                return res.json({ success: false, msg: "You could not delete this pdf..", data: { PDFs: null } });
+
+            }
+
+        } else {
+            return res.json({ success: false, msg: "Could not found pdf,Pdf not available..", data: { PDFs: null } });
+        }
+
+    } catch (error) {
+        console.log('error in finding Pdf', error);
+        return res.json({ success: false, msg: "Internal server Error..", data: null });
+
+    }
+}
--- routes/pdf.js
+++ routes/pdf.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const pdfController = require('../controllers/pdf_controller');
 
 router.post('/uploads', passport.authenticate('jwt', { session: false }), pdfController.uploadPDF);
-
+router.get('/loads/:pdf_id', passport.authenticate('jwt', { session: false }), pdfController.loadPDF);
+router.get('/posts', passport.authenticate('jwt', { session: false }), pdfController.listOfPdf);
+router.get('/downloads/:pdf_id', passport.authenticate('jwt', { session: false }), pdfController.downloadPDF);
+router.delete('/delete/:pdf_id', passport.authenticate('jwt', { session: false }), pdfController.deletePDF);
 
 module.exports = router;
